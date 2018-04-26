26ddd3481b1eadb6059c0bc5374c3972808df586
Roberth Loaiza <pauloaiza@hotmail.es>
2018-04-26 14:31:16 +0200

    lexical analyzer

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,8 @@
 import {Component} from '@angular/core';
+import Lex = require('lexical-parser');
 import {Units} from './unit';
+import {error} from 'util';
+
 
 @Component({
   selector: 'app-home',
@@ -21,10 +24,44 @@ export class HomeComponent {
   */
 
   onEnter(value: string) {
-    const parse = value.split('#new', 1);
-    const unit = new Units().names(parse[0]);
-    unit.toJson();
-    console.log(unit.toJson());
-  }
+// You can specify an exact string or a regex for the token
+    const tokenMatchers = [
+      'new', '#',
+      ['integer', /[0-9]+/],
+      ['id', /[a-zA-Z][a-zA-Z0-9]*/]
+    ];
 
+    const ignorePattern = '[\n\s \t]+';
+
+    const lex = new Lex(value, tokenMatchers, ignorePattern);
+    const id = lex.nextToken();
+    try {
+      if (id['name'] !== 'id') {
+       throw error();
+      } else {
+        const sharp = lex.nextToken();
+        if (sharp['name'] !== '#') {
+          throw error();
+        } else {
+          const news = lex.nextToken();
+          if (news['name'] === 'new') {
+            console.log('exito' + id['lexeme']);
+            const unit = new Units().names(id['lexeme']);
+            unit.toJson();
+            console.log(unit.toJson());
+          }
+        }
+      }
+    } catch (err) {
+      // Error handling
+      if (err.code === 'LEXICAL_ERROR') {
+        console.log(`\n${err.message}\n`);
+        console.log(`Position: ${err.position}`);
+        console.log(`Character: ${err.character}`);
+        console.log(`Nearby code: ${err.nearbyCode}`);
+      } else {
+        console.log('Error Sintactico');
+      }
+    }
+  }
 }
