9036a821c0b60040bc57123a7ea6151be5c87631
Roberth Loaiza <pauloaiza@hotmail.es>
2018-05-03 10:53:15 +0200

    Validate values

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -80,27 +80,26 @@ export class HomeComponent {
     const lex = new Lex(code, tokenMatchers, ignorePattern);
     const id = lex.nextToken();
     try {
-      if (id['name'] !== 'id') {
+      if (id['type'] !== 'id') {
         throw error();
       } else {
         const sharp = lex.nextToken();
-        if (sharp['name'] !== '#') {
+        if (sharp['type'] !== '#') {
           throw error();
         } else {
           const news = lex.nextToken();
-          if (news['name'] === 'new') {
-            const unit = new Units().names(id['lexeme']);
+          if (news['type'] === 'new') {
+            const unit = new Units().names(id['value']);
             this.createUnit(unit);
+          } else {
+            throw error();
           }
         }
       }
     } catch (err) {
       // Error handling
       if (err.code === 'LEXICAL_ERROR') {
-        console.log(`\n${err.message}\n`);
-        console.log(`Position: ${err.position}`);
-        console.log(`Character: ${err.character}`);
-        console.log(`Nearby code: ${err.nearbyCode}`);
+        this.snackBar.open(err.message, 'X');
       } else {
         this.snackBar.open(err, 'X', {
           duration: 8000
