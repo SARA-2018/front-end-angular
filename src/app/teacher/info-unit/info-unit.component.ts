da9d960a1e2bd273953c4a6be064f8d839a9ac6c
Chinegua <ai.chinegua@gmail.com>
2018-06-05 13:16:02 +0200

    adding lesson & session view

--- src/app/teacher/info-unit/info-unit.component.ts
+++ src/app/teacher/info-unit/info-unit.component.ts
@@ -8,14 +8,17 @@ import { Component} from '@angular/core';
 
 export class InfoUnitComponent {
 
-  public values: number[] = [0];
-  public values2: number[] = [0];
+  public sessions: number[] = [1];
+  public lessons: number[] = [0];
 
   constructor() {
   }
+  toArray(n: number): number[] {
+    return Array(n);
+  }
 
   visibilityLesson() {
-    this.values.push(this.values.length + 1);
+    this.sessions.push(1);
     /*
     const lesson = document.getElementById('card_lesson');
     const validate = lesson.style.display;
@@ -26,8 +29,10 @@ export class InfoUnitComponent {
     }*/
   }
 
-  visibility() {
-    this.values2.push(this.values2.length + 1);
+  visibility(sessionId: number) {
+    this.sessions[sessionId] = this.sessions[sessionId] + 1;
+    console.log(this.sessions);
+    // this.lessons.push(this.lessons.length + 1);
     /*const content = document.getElementById('card_cont_lesson');
     const validate = content.style.display;
     if (validate === 'none' || validate === '') {
