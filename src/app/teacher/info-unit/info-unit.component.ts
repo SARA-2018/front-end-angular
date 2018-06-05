bd8627871f273538298b76cd0fda03e91bc817fe
Chinegua <ai.chinegua@gmail.com>
2018-06-05 14:41:43 +0200

    vista itinerarios sessiones y lecciones

--- src/app/teacher/info-unit/info-unit.component.ts
+++ src/app/teacher/info-unit/info-unit.component.ts
@@ -9,7 +9,7 @@ import { Component} from '@angular/core';
 export class InfoUnitComponent {
 
   public sessions: number[] = [1];
-  public lessons: number[] = [0];
+  public itinerarys: number[] = [0];
 
   constructor() {
   }
@@ -19,26 +19,12 @@ export class InfoUnitComponent {
 
   visibilityLesson() {
     this.sessions.push(1);
-    /*
-    const lesson = document.getElementById('card_lesson');
-    const validate = lesson.style.display;
-    if (validate === 'none' || validate === '') {
-      lesson.style.display = 'block';
-    } else {
-      lesson.style.display = 'none';
-    }*/
   }
 
   visibility(sessionId: number) {
     this.sessions[sessionId] = this.sessions[sessionId] + 1;
-    console.log(this.sessions);
-    // this.lessons.push(this.lessons.length + 1);
-    /*const content = document.getElementById('card_cont_lesson');
-    const validate = content.style.display;
-    if (validate === 'none' || validate === '') {
-      content.style.display = 'block';
-    } else {
-      content.style.display = 'none';
-    }*/
+  }
+  addItinerary() {
+    this.itinerarys.push(0);
   }
 }
