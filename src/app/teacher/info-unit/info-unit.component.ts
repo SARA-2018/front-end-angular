267078a89f455a68d33ba1daa940959b4c63265f
Roberth Loaiza <pauloaiza@hotmail.es>
2018-06-05 11:17:05 +0200

    Init command load commands

--- src/app/teacher/info-unit/info-unit.component.ts
+++ src/app/teacher/info-unit/info-unit.component.ts
@@ -8,26 +8,32 @@ import { Component} from '@angular/core';
 
 export class InfoUnitComponent {
 
+  public values: number[] = [0];
+  public values2: number[] = [0];
+
   constructor() {
   }
 
   visibilityLesson() {
+    this.values.push(this.values.length + 1);
+    /*
     const lesson = document.getElementById('card_lesson');
     const validate = lesson.style.display;
     if (validate === 'none' || validate === '') {
       lesson.style.display = 'block';
     } else {
       lesson.style.display = 'none';
-    }
+    }*/
   }
 
   visibility() {
-    const content = document.getElementById('card_cont_lesson');
+    this.values2.push(this.values2.length + 1);
+    /*const content = document.getElementById('card_cont_lesson');
     const validate = content.style.display;
     if (validate === 'none' || validate === '') {
       content.style.display = 'block';
     } else {
       content.style.display = 'none';
-    }
+    }*/
   }
 }
