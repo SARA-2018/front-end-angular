b62ee3d16a9f588678c7852b3b4f87da6a56dd40
raquelsr <raquelserranor@hotmail.com>
2018-06-07 14:13:38 +0200

    Lexico Open

--- src/app/teacher/graph-unit/models/commands/open-unit.model.ts
+++ src/app/teacher/graph-unit/models/commands/open-unit.model.ts
@@ -2,16 +2,18 @@ import { Command } from './command.model';
 import { UnitService } from '../../../graph-unit/services/unit.service';
 import { Observable } from 'rxjs/Observable';
 
-export class SearchFriendUnit extends Command {
+export class OpenUnit extends Command {
 
   private codeTopUnit;
 
   constructor(codeTopUnit: number) {
     super();
     this.codeTopUnit = codeTopUnit;
+    console.log('construye open');
   }
 
   execute(unitService: UnitService): Observable<any> {
+    console.log('ejecutado open');
     return unitService.getFriendsByCode(this.codeTopUnit);
   }
 
