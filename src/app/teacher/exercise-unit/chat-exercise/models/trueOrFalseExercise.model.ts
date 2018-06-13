f62611219739238b2aca301733574e2185e16a64
Chinegua <ai.chinegua@gmail.com>
2018-06-13 14:43:40 +0200

    adding true or false question

--- src/app/teacher/exercise-unit/chat-exercise/models/trueOrFalseExercise.model.ts
+++ src/app/teacher/exercise-unit/chat-exercise/models/trueOrFalseExercise.model.ts
@@ -1,16 +1,42 @@
+import { Solution } from '../../../shared/solution.model';
+import { Exercise } from '../../../shared/exercise.model';
+
 export class TrueOrFalseExercise {
-    messageArray: string[];
-    exercise: string;
-    solutions: string[];
+    objJson;
+    exercise: Exercise;
+    solution: Solution;
 
     constructor(json: string) {
-        this.messageArray = messageArray;
-        this.exercise = exercise;
-        this.solutions = solutions;
+        this.objJson = JSON.parse(json);
+        this.exercise = new Exercise(this.objJson.name);
+        this.solution = this.pickSolution(this.objJson.solutions);
+        this.exercise.addSolution(this.solution);
+        // this.generateExercise(this.solutions);
     }
-
-    generateExercise(): string[]{
-        this.messageArray.push(this.exercise);
+    pickSolution(jsonSolution): Solution {
+        console.log(jsonSolution[this.getRandom(0, jsonSolution.length )]);
+        const solutionChoose = jsonSolution[this.getRandom(0, jsonSolution.length)];
+        return new Solution(solutionChoose.text, solutionChoose.isCorrect );
+    }
+    getRandom(min, max) {
+        return Math.round(Math.random() * (max - min) + min);
+    }
+    checkIsCorrect(response: string): boolean {
+        if ( response === 'Verdadero' ) {
+            if (this.solution.getIsCorrect) {
+                return true;
+            } else {
+                return false;
+            }
+        } else if ( response === 'False') {
+            if (!this.solution.getIsCorrect) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
     }
 
 }
