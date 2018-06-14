60244d9c794da7d80d644a46b3352bb16b98919c
raquelsr <raquelserranor@hotmail.com>
2018-06-14 09:51:59 +0200

    Scroll

--- src/app/teacher/exercise-unit/chat-exercise/chat-exercise.component.ts
+++ src/app/teacher/exercise-unit/chat-exercise/chat-exercise.component.ts
@@ -17,5 +17,7 @@ export class ChatExerciseComponent {
   constructor() {
     this.messages.push(new Message('¡Bienvenido pringaete!', RolMessage.TEACHER));
     this.messages.push(new Message('Qué es lo que quieres?', RolMessage.STUDENT));
+    this.messages.push(new Message('¿Sabe usted qué es lo que quiero?', RolMessage.TEACHER));
+    this.messages.push(new Message('¡¡ LA TARJETA DEL HORMIGUERO !!', RolMessage.STUDENT));
   }
 }
--- src/app/teacher/exercise-unit/chat-exercise/message-item/message.model.ts
+++ src/app/teacher/exercise-unit/chat-exercise/message-item/message.model.ts
@@ -3,21 +3,15 @@ import { RolMessage } from './rol-message.enum';
 export class Message {
 
     private text: string;
-    private date: Date;
     private rol: RolMessage;
 
     constructor(text: string, rol: RolMessage) {
         this.text = text;
-        this.date = new Date();
         this.rol = rol;
     }
 
     getText(): string {
         return this.text;
-    }
-
-    getDate(): Date {
-        return this.date;
     }
 
     getRol(): RolMessage {
