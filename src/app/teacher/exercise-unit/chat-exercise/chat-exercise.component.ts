7756c270e882d521ce264d42ee14d4aca78644eb
raquelsr <raquelserranor@hotmail.com>
2018-06-14 10:32:16 +0200

    Refactoring

--- src/app/teacher/exercise-unit/chat-exercise/chat-exercise.component.ts
+++ src/app/teacher/exercise-unit/chat-exercise/chat-exercise.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
-import { Message } from './message-item/message.model';
-import { RolMessage } from './message-item/rol-message.enum';
+import { Message } from './message/message.model';
+import { RolMessage } from './message/rol-message.enum';
 import { TrueOrFalseExercise } from './models/true-or-false-exercise.model';
 import {TextExercise} from './models/text-exercise.model';
 
@@ -19,7 +19,7 @@ export class ChatExerciseComponent {
   constructor() {
     const exercise: TextExercise = new TextExercise('{ "name":"¿En que año murio Cristobal Colon?", "solutions":[{ "text": "ayer", "isCorrect": true, "justification": [] }]}');
     const TF: TrueOrFalseExercise = new TrueOrFalseExercise('{ "name":"Prueba", "solutions":[ { "text": "Solucion", "isCorrect": true, "justification": [] }, { "text": "Solucion2", "isCorrect": true, "justification": [] }] }');
-    
+
     this.messages.push(new Message('¡Bienvenido pringaete!', RolMessage.TEACHER));
     this.messages.push(new Message('Qué es lo que quieres?', RolMessage.STUDENT));
     this.messages.push(new Message('¿Sabe usted qué es lo que quiero?', RolMessage.TEACHER));
