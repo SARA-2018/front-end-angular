8f272ea8768004a087058e1a94b6a36e1f68f6b6
raquelsr <raquelserranor@hotmail.com>
2018-06-15 10:59:38 +0200

    Model Exercise

--- src/app/teacher/exercise-unit/chat-exercise/chat-exercise.component.ts
+++ src/app/teacher/exercise-unit/chat-exercise/chat-exercise.component.ts
@@ -1,10 +1,13 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Message } from './message/message.model';
 import { RolMessage } from './message/rol-message.enum';
 import { TrueOrFalseExercise } from './models/true-or-false-exercise.model';
-import { TextExercise} from './models/text-exercise.model';
+import { TextExercise } from './models/text-exercise.model';
 import { MultipleChoise } from './models/multiple-choise.model';
-import { FillExercise} from './models/fill-exercise.model';
+import { FillExercise } from './models/fill-exercise.model';
+import { Exercise } from '../../shared/exercise.model';
+import { Solution } from '../../shared/solution.model';
+import { Justification } from '../../shared/justification.model';
 
 @Component({
   selector: 'app-chat-exercise',
@@ -13,16 +16,17 @@ import { FillExercise} from './models/fill-exercise.model';
 
 })
 
-export class ChatExerciseComponent {
+export class ChatExerciseComponent implements OnInit {
 
   @Input('messages')
   private messages: Message[] = [];
+  private exercise: Exercise;
 
   constructor() {
-    const TE: TextExercise = new TextExercise('{ "name":"¿En que año murio Cristobal Colon?", "solutions":[{ "text": "1506", "isCorrect": true, "justification": [] }]}');
-    const TF: TrueOrFalseExercise = new TrueOrFalseExercise('{ "name":"Prueba", "solutions":[ { "text": "Solucion", "isCorrect": true, "justification": [] }, { "text": "Solucion2", "isCorrect": true, "justification": [] }] }');
-    const FE: FillExercise = new FillExercise('{ "name":"¿En que año murio __________ Colon?", "solutions":[ { "text": "Cristobal", "isCorrect": true, "justification": [] }, { "text": "Antonio", "isCorrect": false, "justification": [] }] }');
-    const MC: MultipleChoise = new MultipleChoise('{ "name":"Prueba", "solutions":[ { "text": "Solucion", "isCorrect": true, "justification": [] }, { "text": "Solucion2", "isCorrect": true, "justification": [] },{ "text": "Solucion3", "isCorrect": true, "justification": [] }, { "text": "Solucion4", "isCorrect": true, "justification": [] },{ "text": "Solucion5", "isCorrect": true, "justification": [] }, { "text": "Solucion6", "isCorrect": true, "justification": [] }] }');
+    // const TE: TextExercise = new TextExercise('{ "name":"¿En que año murio Cristobal Colon?", "solutions":[{ "text": "1506", "isCorrect": true, "justification": [] }]}');
+    // const TF: TrueOrFalseExercise = new TrueOrFalseExercise('{ "name":"Prueba", "solutions":[ { "text": "Solucion", "isCorrect": true, "justification": [] }, { "text": "Solucion2", "isCorrect": true, "justification": [] }] }');
+    // const FE: FillExercise = new FillExercise('{ "name":"¿En que año murio __________ Colon?", "solutions":[ { "text": "Cristobal", "isCorrect": true, "justification": [] }, { "text": "Antonio", "isCorrect": false, "justification": [] }] }');
+    // const MC: MultipleChoise = new MultipleChoise('{ "name":"Prueba", "solutions":[ { "text": "Solucion", "isCorrect": true, "justification": [] }, { "text": "Solucion2", "isCorrect": true, "justification": [] },{ "text": "Solucion3", "isCorrect": true, "justification": [] }, { "text": "Solucion4", "isCorrect": true, "justification": [] },{ "text": "Solucion5", "isCorrect": true, "justification": [] }, { "text": "Solucion6", "isCorrect": true, "justification": [] }] }');
 
     this.messages.push(new Message('¡Bienvenido pringaete!', RolMessage.TEACHER));
     this.messages.push(new Message('Qué es lo que quieres?', RolMessage.STUDENT));
@@ -30,6 +34,28 @@ export class ChatExerciseComponent {
     this.messages.push(new Message('¡¡ LA TARJETA DEL HORMIGUERO !!', RolMessage.STUDENT));
   }
 
+  ngOnInit() {
+    // GET Peticion
+    const json = '{ "name":"Prueba", "solutions":[ { "text": "Solucion", "isCorrect": true, "justifications": [ {"text": " Justificacion1", "isCorrect": true}, {"text": " Justificacion2", "isCorrect": true} ] }, { "text": "Solucion2", "isCorrect": true, "justifications": [ ] },{ "text": "Solucion3", "isCorrect": true, "justifications": [ ] }, { "text": "Solucion4", "isCorrect": true, "justifications": [ ] },{ "text": "Solucion5", "isCorrect": true, "justifications": [ ] }, { "text": "Solucion6", "isCorrect": true, "justifications": [ ] }] }';
+    this.createModels(json);
+  }
+
+  createModels(json: string) {
+    const objJson = JSON.parse(json);
+    this.exercise = new Exercise(objJson.name);
+    if (objJson.solutions.length > 0) {
+      for (let i = 0; i < objJson.solutions.length; i++) {
+        const solution = new Solution(objJson.solutions[i].text, objJson.solutions[i].isCorrect);
+        if (objJson.solutions[i].justifications.length > 0 ) {
+          for (let j = 0; j < objJson.solutions[i].justifications.length; j++) {
+            solution.addJustification(new Justification(objJson.solutions[i].justifications[j].text,
+              objJson.solutions[i].justifications[j].isCorrect));
+          }
+        }
+        this.exercise.addSolution(solution);
+      }
+    }
+  }
 
   send(text: string) {
     this.messages.push(new Message(text, RolMessage.STUDENT));
