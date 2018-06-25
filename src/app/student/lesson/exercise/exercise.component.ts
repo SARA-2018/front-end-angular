ec5f46ca23cf394ffb974e304f60c81d86182caf
Roberth Loaiza <pauloaiza@hotmail.es>
2018-06-25 14:26:32 +0200

    video

--- src/app/student/lesson/exercise/exercise.component.ts
+++ src/app/student/lesson/exercise/exercise.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import {ExerciseService} from '../../../shared/exercise.service';
+import { ExerciseService } from '../../../shared/exercise.service';
 
 @Component({
     templateUrl: 'exercise.component.html',
