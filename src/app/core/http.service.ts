69141b02b4bf4acaa45817546945800ee64d5077
raquelsr <raquelserranor@hotmail.com>
2018-06-27 10:22:37 +0200

    Core refactor

--- src/app/core/http.service.ts
+++ src/app/core/http.service.ts
@@ -84,6 +84,7 @@ export class HttpService {
     }
 
     patch(endpoint: string, body?: Object): Observable<any> {
+        this.body = body;
         return this.http.patch(HttpService.API_END_POINT + endpoint, body, this.createOptions()).map(
             response => this.extractData(response)).catch(
                 error => {
@@ -123,9 +124,6 @@ export class HttpService {
 
     private handleError(response: Response): any {
         let error: Error;
-        if (response.status === ErrorHttp.UNAUTHORIZED) {
-            // this.logout();
-        }
         try {
             error = {
                 httpError: response.status, exception: response.json().exception,
