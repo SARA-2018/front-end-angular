040b1c21aedcd41e0925756ce4cd973c0820010f
raquelsr <raquelserranor@hotmail.com>
2018-06-27 14:18:00 +0200

    Refactor

--- src/app/shared/models/itinerary.model.ts
+++ src/app/shared/models/itinerary.model.ts
@@ -1,25 +1,17 @@
 import { Formation } from './formation.model';
 
 export class Itinerary extends Formation {
-    private name: string;
     private formations: Formation[];
 
-    constructor(name?: string) {
-        super();
-        this.name = name;
+    constructor(name: string) {
+        super(name);
         this.formations = [];
-    }
-    setName(name: string): Itinerary {
-        this.name = name;
-        return this;
     }
     setFormations(formations: Formation[]): Itinerary {
         this.formations = formations;
         return this;
     }
-    getName(): string {
-        return this.name;
-    }
+
     getFormations(): Formation[] {
         return this.formations;
     }
